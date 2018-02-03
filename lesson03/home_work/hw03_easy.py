716c247af54396c9021c4dcd4bc49b081ef62016
aapanteleev@icloud.com <Q1a2Z3w4S5>
2018-02-03 10:38:20 +0300

    lesson03

--- lesson03/home_work/hw03_easy.py
+++ lesson03/home_work/hw03_easy.py
@@ -4,8 +4,13 @@
 # Округление должно происходить по математическим правилам (0.6 --> 1, 0.4 --> 0).
 # Для решения задачи не используйте встроенные функции и функции из модуля math.
 
+
 def my_round(number, ndigits):
-    pass
+    nd = 10 ** ndigits
+    num = number * nd
+    if num % nd >= 5:
+        num += 1
+    return int(num) / nd
 
 
 print(my_round(2.1234567, 5))
@@ -20,7 +25,18 @@ print(my_round(2.9999967, 5))
 # !!!P.S.: функция не должна НИЧЕГО print'ить
 
 def lucky_ticket(ticket_number):
-    pass
+    str_ticket_number = str(ticket_number)
+    half_sum_left = 0
+    half_sum_right = 0
+    half_len = len(str_ticket_number) // 2
+    if half_len * 2 == len(str_ticket_number):
+        for number in str_ticket_number[:half_len]:
+            half_sum_left = half_sum_left + int(number)
+        for number in str_ticket_number[half_len:]:
+            half_sum_right = half_sum_right + int(number)
+        if half_sum_left == half_sum_right:
+            return True
+    return False
 
 
 print(lucky_ticket(123006))
--- lesson03/home_work/hw03_normal.py
+++ lesson03/home_work/hw03_normal.py
@@ -2,8 +2,13 @@
 # Напишите функцию, возвращающую ряд Фибоначчи с n-элемента до m-элемента.
 # Первыми элементами ряда считать цифры 1 1
 
+
 def fibonacci(n, m):
-    pass
+    temp = [0, 1]
+    for i in range(1, m):
+        temp.append(temp[-2] + temp[-1])
+    return temp[n:]
+
 
 # Задача-2:
 # Напишите функцию, сортирующую принимаемый список по возрастанию.
@@ -12,7 +17,14 @@ def fibonacci(n, m):
 
 
 def sort_to_max(origin_list):
-    pass
+    new_list = list()
+    for number in origin_list:
+        i = 0
+        while i < len(new_list) and number > new_list[i]:
+            i += 1
+        new_list.insert(i, number)
+    return new_list
+
 
 sort_to_max([2, 10, -12, 2.5, 20, -11, 4, 4, 0])
 
@@ -21,7 +33,24 @@ sort_to_max([2, 10, -12, 2.5, 20, -11, 4, 4, 0])
 # Разумеется, внутри нельзя использовать саму функцию filter.
 
 
+def filter_function(x):
+    if x > 0:
+        return x
+    return None
+
+
+def my_filter(origin_list):
+    return_list = []
+    for x in origin_list:
+        if filter_function(x):
+            return_list.append(x)
+    return return_list
+
+
+my_filter([2, 34, 0, -12, 34, -3, 0, 89])
+
 # Задача-4:
 # Даны четыре точки А1(х1, у1), А2(x2 ,у2), А3(x3 , у3), А4(х4, у4).
 # Определить, будут ли они вершинами параллелограмма.
+
 
